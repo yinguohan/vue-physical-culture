0b9c86f0f634a85b71518dd3f43ab8183d928c3a
Charlie-Ping <78915641+Charlie-Ping@users.noreply.github.com>
2023-12-18 12:31:11 +0800

    更新readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# ending-job
+# Chsi-imitate
+Vue + tailwind 仿写学信网主页
+
+期中作业 仅用于练习
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: './',
+  transpileDependencies: true,
+  configureWebpack: {
+    optimization: {
+      minimize: false
+    },
+    resolve: {
+      alias: {
+        'assets': '@/assets'
+      }
+    }
+  }
 })
